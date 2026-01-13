f4387f51d8ff35cde4bfca4a1fb5fb9b2f04bc81
oxedom <oxedom@gmail.com>
2026-01-13 11:48:00 +0200

    aaa

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,9 @@
 alias explorerpath='wslpath -w $(pwd) | clip.exe'
 git config --global alias.add-commit '!git add -A && git commit'
-alias clip='xclip -selection clipboard'
+
+if command -v clip.exe >/dev/null 2>&1; then
+    alias klip='clip.exe'
+elif command -v xclip >/dev/null 2>&1; then
+    alias klip='xclip -selection clipboard'
+fi
+alias clip='klip'
